60637ba19fa80ac71663c1c17d88969698739617
ïmadao <xieao100@163.com>
2016-04-07 16:22:54 +0800

    Initial commit

---
+++ app/src/main/java/hasee/com/loveweather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package hasee.com.loveweather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hasee.com.loveweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
